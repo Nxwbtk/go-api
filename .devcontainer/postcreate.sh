ef4acdb94078af07998b179b933dfaac658e9d34
bsirikam <buntakan0703@gmail.com>
2024-03-01 02:51:16 +0000

    update postcreate

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
 
-PRE='export $(cat '
-POST='/.env.local | xargs)'
-CMD=$PRE$1$POST
-echo $CMD >> ~/.bashrc
-
 apt-get update && apt-get install -y --no-install-recommends postgresql-client
 cd app && go mod download
